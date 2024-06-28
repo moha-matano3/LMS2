8dcc1dfff907803fe79a3b0456fc3b1da612f33a
Mohamed Nyaa Matano <Moha_Matano@Mohas-MacBook-Air.local>
2024-06-28 10:17:49 +0300

    Push notifications final

--- resources/views/emails/reminder_notification.blade.php
+++ resources/views/emails/reminder_notification.blade.php
@@ -7,11 +7,7 @@
 <body>
     <p>Dear {{ $user->name }},</p>
 
-<<<<<<< HEAD
-    <p>This is a reminder that the book titled, "<b>{{$book->book_title}}</b>" you borrowed from Hekima Library was due on {{ $due_date }}. Please return the book as soon as possible.</p>
-=======
     <p>This is a reminder that the book you borrowed titled, {{$book->book_title}} from Hekima Library was due on {{ $due_date }}. Please return the book as soon as possible to avoid any further penalties.</p>
->>>>>>> 8926d30645d2e56ef1407863373001f79f2e495c
 
     <p>Thank you,<br>Hekima Library</p>
 </body>
